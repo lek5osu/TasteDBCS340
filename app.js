bce2323df4ce13c6cc0d5ee902efcab5e59ee945
ScoobyJoo <88563461+ScoobyJoo@users.noreply.github.com>
2024-12-02 08:57:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-const PORT = 9244;                  // Set a port number at the top so it's easy to change in the future
+const PORT = 9245;                  // Set a port number at the top so it's easy to change in the future
 app.use(express.json());  
 app.use(express.static('public')); // dev note: this was causing so many issues
 
@@ -119,6 +119,37 @@ app.post('/add-dish', function(req, res) {
     });
 });
 
+// Add Restaurant and Dish
+app.post('/add-RestaurantDish', function(req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    query1 = `INSERT INTO Restaurants_Dishes (RestaurantID, DishID) VALUES ('${data.RestaurantID}', '${data.DishID}')`;
+
+    db.pool.query(query1, function(error, rows, fields) {
+        // Check if there was an error with the insertion
+        if (error) {
+            console.log(error);  // Log the error for debugging
+            res.sendStatus(400);
+        } else {
+            // If the insert was successful, fetch the updated list of restaurants
+            query2 = `SELECT * FROM Restaurants_Dishes`;
+
+            // Fetch the updated list of restaurants
+            db.pool.query(query2, function(error, rows, fields) {
+                // Check if there was an error with the SELECT query
+                if (error) {
+                    console.log(error);  // Log any error
+                    res.sendStatus(400);
+                } 
+                else {
+                    res.send(rows);
+                }
+            });
+        }
+    });
+});
+
 // Update RestaurantsDishes
 app.put('/put-restaurant-ajax', function(req, res, next) {
     let data = req.body;
@@ -136,8 +167,63 @@ app.put('/put-restaurant-ajax', function(req, res, next) {
     })
 });
 
+// Delete Dishes
+app.delete('/delete-dish-ajax/', function(req,res,next){
+    let data = req.body;
+    let DishID = parseInt(data.DishID);
+    let deleteRestaurantsDishes = `DELETE FROM Restaurants_Dishes WHERE DishID = ?`;
+    let deleteDishes = `DELETE FROM Dishes WHERE DishID = ?`;
+        // Run the 1st query
+        db.pool.query(deleteRestaurantsDishes, [DishID], function(error, rows, fields){
+            if (error){
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error);
+                console.log("Request Body:", req.body);
+                res.sendStatus(400);
+            } else{
+                // Run the second query
+                db.pool.query(deleteDishes, [DishID], function(error, rows, fields) {
+                if (error){
+                    console.log(error);
+                    res.sendStatus(400);
+                } else{
+                    res.sendStatus(204);
+                }
+            })
+        }
+    })
+});
+
+// Delete Restaurants
+app.delete('/delete-restaurant-ajax/', function(req,res,next){
+    let data = req.body;
+    let RestaurantID = parseInt(data.RestaurantID);
+    let deleteRestaurantsDishes = `DELETE FROM Restaurants_Dishes WHERE RestaurantID = ?`;
+    let deleteRestaurants = `DELETE FROM Restaurants WHERE RestaurantID = ?`;
+        // Run the 1st query
+        db.pool.query(deleteRestaurantsDishes, [RestaurantID], function(error, rows, fields){
+            if (error) {
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error);
+                console.log("Request Body:", req.body);
+                res.sendStatus(400);
+            }  
+            else{
+                // Run the second query
+                db.pool.query(deleteRestaurants, [RestaurantID], function(error, rows, fields) {
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    } else {
+                        res.sendStatus(204);
+                    }
+                })
+            }
+        })
+});
+
 // Delete RestaurantsDishes
-app.delete('/delete-restaurant-ajax', function(req, res, next){
+app.delete('/delete-restaurantsDishes-ajax', function(req, res, next){
     let data = req.body;
     let RestaurantDishID = parseInt(data.RestaurantDishID);
     let deleteRestaurantsDishes = `DELETE FROM RestaurantsDishes WHERE RestaurantDishID = ?`;
