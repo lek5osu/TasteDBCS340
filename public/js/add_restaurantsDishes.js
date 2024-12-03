ffcee0b569e61d86e5c4d26bf5bf1fcf433e2fbd
ScoobyJoo <88563461+ScoobyJoo@users.noreply.github.com>
2024-12-02 16:33:08 -0800

    Completed add_restaurantsDishes.js

--- public/js/add_restaurantsDishes.js
+++ public/js/add_restaurantsDishes.js
@@ -1,27 +1,30 @@
 // Get the objects we need to modify
-let addRestaurantForm = document.getElementById('add-restaurantsDishes-form-ajax');
+let addRestaurantDishForm = document.getElementById("add-RestaurantsDishes-form-ajax");
 
 // Modify the objects we need
-addRestaurantForm.addEventListener("submit", function (e) {
+addRestaurantDishForm.addEventListener("submit", function (e) {
     
     // Prevent the form from submitting
     e.preventDefault();
 
     // Get the form fields from restaurantsDishes.hbs
-    let inputRestaurantName = document.getElementById("input-restaurantsDishes-name");
+    let inputRestaurantID = document.getElementById("input-restaurant-ID");
+    let inputDishID = document.getElementById("input-dish-ID");
 
     
     // Get the values from the form fields
-    let restaurantNameValue = inputRestaurantName.value;
+    let restaurantIDValue = inputRestaurantID.value;
+    let dishIDValue = inputDishID.value;
 
     // Put our data we want to send in a javascript object
     let data = {
-        RestaurantName: restaurantNameValue
+        RestaurantID: restaurantIDValue,
+        DishID: dishIDValue
     };
 
     // Setup our AJAX request
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/add-restaurantsDishes", true);
+    xhttp.open("POST", "/add-RestaurantDish", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
     // Tell our AJAX request how to resolve
@@ -32,7 +35,8 @@ addRestaurantForm.addEventListener("submit", function (e) {
             addRowToTable(xhttp.response);
 
             // Clear the input fields for another transaction
-            inputRestaurantName.value = '';
+            inputRestaurantID.value = '';
+            inputDishID.value = '';
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
             console.log("There was an error with the input.");
@@ -40,15 +44,16 @@ addRestaurantForm.addEventListener("submit", function (e) {
     }
 
     // Send the request and wait for the response
+    console.log(data);
     xhttp.send(JSON.stringify(data));
 })
 
 // Creates a single row from an Object representing a single record from
-// the Restaurants table
+// the Restaurants&Dishes table
 addRowToTable = (data) => {
 
     // Get a reference to the current table on the page
-    let currentTable = document.getElementById("restaurantsDishes-table");
+    let currentTable = document.getElementById("Restaurants-Dishes-Table");
 
     // Get the location where we should insert the new row (end of table)
     let newRowIndex = currentTable.rows.length;
@@ -70,7 +75,7 @@ addRowToTable = (data) => {
     editLink.innerText = "Edit";
     // Add the event listener for the Edit button (you can define the edit function)
     editLink.addEventListener("click", function() {
-        editRestaurant(newRow.RestaurantID); // Replace with your edit function
+        editRestaurantDish(newRow.RestaurantDishID); // Replace with your edit function
     });
 
     // Create the Delete link
@@ -79,23 +84,25 @@ addRowToTable = (data) => {
     deleteLink.innerText = "Delete";
     // Add the event listener for the Delete button (you can define the delete function)
     deleteLink.addEventListener("click", function() {
-        deleteRestaurant(newRow.RestaurantID); // Replace with your delete function
+        deleteRestaurantDish(newRow.RestaurantDishID); // Replace with your delete function
     });
 
 
      // Create the data cells for RestaurantID and RestaurantName
+    let restaurantDishIDCell = document.createElement("TD");
     let restaurantIDCell = document.createElement("TD");
-    let restaurantNameCell = document.createElement("TD");
+    let dishIDCell = document.createElement("TD");
+    restaurantDishIDCell.innerText = newRow.RestaurantDishID;
     restaurantIDCell.innerText = newRow.RestaurantID;
-    restaurantNameCell.innerText = newRow.RestaurantName;
+    dishIDCell.innerText = newRow.DishID;
 
     // Append all cells to the row
     editCell.appendChild(editLink);
     deleteCell.appendChild(deleteLink);
     row.appendChild(editCell);
     row.appendChild(deleteCell);
+    row.appendChild(restaurantDishIDCell);
     row.appendChild(restaurantIDCell);
-    row.appendChild(restaurantNameCell);
+    row.appendChild(dishIDCell);
     currentTable.appendChild(row);
-
 }
