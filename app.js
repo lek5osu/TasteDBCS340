d0d98e7d87d8b510d67fbee724519b67655ab1a7
le <khoid.le11@gmail.com>
2024-12-02 18:59:25 -0600

    Implemented ADDS and SELECT
    SELECT and ADDS functional for users.
    SELECT and ADDS functional for Reviews.
    *Reviews currently hardcode FKs instead of FK values as intended. WIll fix in future

--- app.js
+++ app.js
@@ -55,6 +55,96 @@ app.get('/restaurantsDishes', function(req, res) {
     });
 });
 
+// Route for Users Browse
+app.get('/users', function(req, res) {
+    let query1 = 'SELECT * FROM Users;';
+
+    db.pool.query(query1, function(error, results1) {
+        if (error) throw error;
+            res.render('users', {
+                data1: results1,  
+                });
+            });
+        });
+
+// Route for Reviews Browse
+app.get('/reviews', function (req, res) {
+    // Query to retrieve Reviews
+    let query = `
+        SELECT 
+            Reviews.ReviewID,
+            CONCAT(Users.UserFirstName, ' ', Users.UserLastName) AS UserName,
+            Restaurants.RestaurantName,
+            Dishes.DishName,
+            DATE_FORMAT(Reviews.ReviewDate, '%m-%d-%Y') AS ReviewDate,
+            Reviews.ReviewComment,
+            Reviews.Taste,
+            Reviews.Presentation
+        FROM Reviews
+        JOIN Users ON Reviews.UserID = Users.UserID
+        JOIN Restaurants ON Reviews.RestaurantID = Restaurants.RestaurantID
+        JOIN Dishes ON Reviews.DishID = Dishes.DishID;
+    `;
+
+    // Dropdown Queries
+    let queryRestaurants = `SELECT RestaurantID, RestaurantName FROM Restaurants;`;
+    let queryDishes = `SELECT DishID, DishName FROM Dishes;`;
+    let queryUsers = `SELECT UserID, CONCAT(UserFirstName, ' ', UserLastName) AS UserName FROM Users;`;
+
+    // Run the main Reviews query
+    db.pool.query(query, function (error, rows, fields) {
+        if (error) throw error;
+
+        // Save the Reviews data
+        let reviews = rows;
+        console.log("Reviews:", reviews);
+        // Run Restaurants query
+        db.pool.query(queryRestaurants, function (error, rows, fields) {
+            if (error) throw error;
+
+            // Save the Restaurants data
+            let restaurants = rows;
+            console.log("Restaurants:", restaurants);
+            // Run Dishes query
+            db.pool.query(queryDishes, function (error, rows, fields) {
+                if (error) throw error;
+
+                // Save the Dishes data
+                let dishes = rows;
+                console.log("Dishes:", dishes);
+                // Run Users query
+                db.pool.query(queryUsers, function (error, rows, fields) {
+                    if (error) throw error;
+
+                    // Save the Users data
+                    let users = rows;
+                    console.log("Users:", users);
+                    // Render the reviews page with all the data
+                    // REF: https://stackoverflow.com/questions/54388669/mapping-data-with-dynamic-variables
+                    // used to map entity attributes to work with "this.id" and "this.name" in hbs
+                    return res.render('reviews', {
+                        data1: reviews,
+                        restaurants: restaurants.map(r => ({ id: r.RestaurantID, name: r.RestaurantName })),
+                        dishes: dishes.map(d => ({ id: d.DishID, name: d.DishName })),
+                        users: users.map(u => ({ id: u.UserID, name: u.UserName }))
+                    });
+                });
+            });
+        });
+    });
+});
+
+
+// Route for RestaurantsAddresses Browse
+app.get('/restaurantsAddresses', function(req, res) {
+    let query1 = 'SELECT * FROM Restaurant_Address;';
+
+    db.pool.query(query1, function(error, results) {
+        if (error) throw error;
+        res.render('restaurantsAddresses', {data1: results});
+    });
+});
+
 // Add Restaurant Route (POST)
 app.post('/add-restaurant', function(req, res) {
     // Capture the incoming data and parse it back to a JS object
@@ -108,6 +198,36 @@ app.post('/add-dish', function(req, res) {
             db.pool.query(query2, function(error, rows, fields) {
                 // Check if there was an error with the SELECT query
                 if (error) {
+                    console.log(error);  
+                    res.sendStatus(400);
+                } 
+                else {
+                    res.send(rows);
+                }
+            });
+        }
+    });
+});
+
+// Add User Route (POST)
+app.post('/add-user', function(req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+    query1 = `INSERT INTO Users (UserFirstName, UserLastName, UserEmail)  VALUES ('${data.UserFirstName}', '${data.UserLastName}', '${data.UserEmail}')`;
+    db.pool.query(query1, function(error, rows, fields) {
+        // Check if there was an error with the insertion
+        if (error) {
+            console.log(error);  // Log the error for debugging
+            res.sendStatus(400);
+        } 
+        else {
+            // If the insert was successful, fetch the updated list of restaurants
+            query2 = `SELECT * FROM Users;`;
+
+            // Fetch the updated list of restaurants
+            db.pool.query(query2, function(error, rows, fields) {
+                // Check if there was an error with the SELECT query
+                if (error) {
                     console.log(error);  // Log any error
                     res.sendStatus(400);
                 } 
@@ -118,6 +238,40 @@ app.post('/add-dish', function(req, res) {
         }
     });
 });
+
+// Add Review Route (POST)
+app.post('/add-review', function(req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    query1 = `INSERT INTO Reviews (RestaurantID, DishID, UserID, ReviewDate, ReviewComment, Taste, Presentation) 
+              VALUES ('${data.RestaurantID}', '${data.DishID}', '${data.UserID}', '${data.ReviewDate}', '${data.ReviewComment}', '${data.Taste}', '${data.Presentation}')`;
+
+    db.pool.query(query1, function(error, rows, fields) {
+        // Check if there was an error with the insertion
+        if (error) {
+            console.log(error);  // Log the error for debugging
+            res.sendStatus(400);
+        } 
+        else {
+            // If the insert was successful, fetch the updated list of reviews
+            query2 = `SELECT * FROM Reviews;`;
+
+            // Fetch the updated list of reviews
+            db.pool.query(query2, function(error, rows, fields) {
+                // Check if there was an error with the SELECT query
+                if (error) {
+                    console.log(error);  
+                    res.sendStatus(400);
+                } 
+                else {
+                    res.send(rows);
+                }
+            });
+        }
+    });
+});
+
 
 // Update RestaurantsDishes
 app.put('/put-restaurant-ajax', function(req, res, next) {
